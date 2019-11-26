ee1b69ac5d2b5ec76e5ad185084d4634fcba2990
D119 <contact@moonbear.cn>
2019-11-26 23:34:34 +0800

    修改首页

--- public/index.js
+++ public/index.js
@@ -1,1 +1,7 @@
-console.log('Hello world from client-side JS!')
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?b191cbecb1f48c1efc01fd0f34194289";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+})();
